8830bdf25fc403ff35ae1bd3ead314e4f0666db2
Greg131 <gregoire_argenton@yahoo.fr>
2015-08-05 17:02:22 +0200

    update

--- Script.R
+++ Script.R
@@ -1,1 +1,11 @@
-setwd("~/Dropbox/Datascience/Université Johns-Hopkins/Obtenir et trier les données/Repo/get-clean-data")
+# ----------------------------------------------------------
+# Script week 1
+# ----------------------------------------------------------
+
+setwd("~/Dropbox/Datascience/Université Johns-Hopkins/Obtenir et trier les données")
+# Absolute setwd("../") ou relative setwd("./") ou ..~/ relative au user...
+getwd() #       Get working directory
+
+if (!file.exists("data"))       {
+        dir.create("data")
+}
