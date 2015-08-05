5cb5d5bcbed65caaf8a269616243268c6aba39cd
Greg131 <gregoire_argenton@yahoo.fr>
2015-08-05 17:34:23 +0200

    update

--- Script.R
+++ Script.R
@@ -9,3 +9,33 @@ getwd() #       Get working directory
 if (!file.exists("data"))       {
         dir.create("data")
 }
+# ----------------------------------------------------------
+#       Downloading data from internet : 
+#       download.file(Url, destile, method)
+# ----------------------------------------------------------
+
+#       Ex Baltimore fixed camera data
+fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
+download.file(fileUrl, destfile = "./data/cameras.csv", method = "curl")
+dateDownloaded <- date()
+dateDownloaded
+
+#       If the url starts with http ... ok, 
+#                       https -> need to set method="curl" with MAC
+
+# ----------------------------------------------------------
+#       Reading files : 
+#       read.table() : flexible, robusf, req more param, slow read data in RAM big data cause pb 
+#               file, header, sep; row names, nrows
+#       read.csv(), read.csv2()
+# ----------------------------------------------------------
+?read.table()
+
+#       cameraData <- read.table("./data/cameras.csv")
+cameraData <- read.table("./data/cameras.csv", sep=",", header = TRUE)
+cameraData <- read.csv("./data/cameras.csv")    # default sep "," header TRUE
+
+#       quote, quote="" means no quotes (often resolves flag file with 'or ")
+#       na.strings set caractere for missing value
+#       nrows - how many rows to read
+#       skip number of lines to skip before starting to read
