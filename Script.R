a41a9aeb505617b926ca5e4526170a24cb400764
Greg131 <gregoire_argenton@yahoo.fr>
2015-08-05 17:55:49 +0200

    update

--- Script.R
+++ Script.R
@@ -16,9 +16,7 @@ if (!file.exists("data"))       {
 
 #       Ex Baltimore fixed camera data
 fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
-download.file(fileUrl, destfile = "./data/cameras.csv", method = "curl")
-dateDownloaded <- date()
-dateDownloaded
+
 
 #       If the url starts with http ... ok, 
 #                       https -> need to set method="curl" with MAC
@@ -39,3 +37,31 @@ cameraData <- read.csv("./data/cameras.csv")    # default sep "," header TRUE
 #       na.strings set caractere for missing value
 #       nrows - how many rows to read
 #       skip number of lines to skip before starting to read
+
+# ----------------------------------------------------------
+#       Reading Excel files : 
+# ----------------------------------------------------------
+
+fileUrl <- https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?accessType=DOWNLOAD
+download.file(fileUrl, destfile = "./data/cameras.xlsx", method = "curl")
+dateDownloaded <- date()
+dateDownloaded
+
+library(xlsx)
+#cameraData <- read.xlsx("./data/cameras.xlsx",sheetIndex=1,header=TRUE)
+#colIndex <- 2:3
+#rowIndex <- 1:4
+#cameraDataSubset <- read.xlsx("./data/cameras.xlsx",sheetIndex=1,header=TRUE, colIndex=colIndex, rowIndex=rowIndex)
+cameraData <- read.xlsx("./data/Baltimore_Fixed_Speed_Cameras.xls",sheetIndex=1,header=TRUE)
+colIndex <- 2:3
+rowIndex <- 1:4
+cameraDataSubset <- read.xlsx("./data/Baltimore_Fixed_Speed_Cameras.xls",sheetIndex=1,header=TRUE, colIndex=colIndex, rowIndex=rowIndex)
+
+
+# ----------------------------------------------------------
+#       Writing Excel files : write.xlsx
+#       read.xls2 faster but instable for subset
+#       XLconnect package ... more options
+# ----------------------------------------------------------
+
+
