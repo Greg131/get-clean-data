cd6060b0ff311602084b8476ec00d9e3cd6b515e
Greg131 <gregoire_argenton@yahoo.fr>
2015-08-13 14:18:20 +0200

    week2 reviewed

--- Script.R
+++ Script.R
@@ -2,7 +2,7 @@
 # Script week 1
 # ----------------------------------------------------------
 
-setwd("~/Dropbox/Datascience/Universit?? Johns-Hopkins/Obtenir et trier les donn??es")
+setwd("~/Dropbox/Datascience/Universite_Johns-Hopkins/Obtenir_et_trier_les_donnees/Repo")
 # Absolute setwd("../") ou relative setwd("./") ou ..~/ relative au user...
 getwd() #       Get working directory
 
@@ -356,3 +356,150 @@ system.time(mean(DT[DT$SEX==1,]$pwgtp15); mean(DT[DT$SEX==2,]$pwgtp15))
 # plus long
 tapply(DT$pwgtp15,DT$SEX,mean)
 system.time(tapply(DT$pwgtp15,DT$SEX,mean))
+
+
+# ----------------------------------------------------------
+#       Week2 : SQL,HDF5
+# ----------------------------------------------------------
+
+library(RMySQL)
+
+ucscDb <- dbConnect(MySQL(), user="genome",
+                    host="genome-mysql.cse.ucsc.edu")
+result <- dbGetQuery(ucscDb,"show databases;"); dbDisconnect(ucscDb); # MySQL commamd passed
+result
+class(result)
+
+hg19 <- dbConnect(MySQL(), user="genome",db="hg19",
+                    host="genome-mysql.cse.ucsc.edu")
+allTables <- dbListTables(hg19)
+length(allTables)
+?dbListTables
+allTables[1:5]
+
+?dbListFields
+dbListFields(hg19,"affyU133Plus2")
+
+dbGetQuery(hg19,"select count(*) from affyU133Plus2")
+affyData <- dbReadTable(hg19, "affyU133Plus2")
+summary(affyData)
+nrow(affyData)
+names(affyData)
+
+query <- dbSendQuery(hg19, "select * from affyU133Plus2 where misMatches between 1 and 3")
+affyMis <- fetch(query)
+nrow(affyMis)
+quantile(affyMis$misMatches)
+
+affMisSmall <- fetch(query, n=10)
+nrow(affMisSmall)
+dbClearResult(query)
+
+dim(affMisSmall)
+
+
+dbDisconnect(hg19)
+
+# ----------------------------------------------------------
+#       HDF5
+# ----------------------------------------------------------
+
+source("http://bioconductor.org/biocLite.R")
+biocLite("rhdfs5")
+library(rhdf5)
+created = h5createFile("example.h5")
+created
+
+created = h5createGroup("example.h5","foo")
+created = h5createGroup("example.h5","baa")
+created = h5createGroup("example.h5","foo/foobaa")
+h5ls("example.h5")
+
+A = matrix(1:10,nr=5,nc=2)
+
+A
+h5write(A,"example.h5,"foo/A"")
+B = array(seq(0.1,2.0,by = 0.1),dim=c(5,2,2))
+B
+h5write(B,"example.h5,"foo/foobaa/B"")
+h5ls("example.h5")
+
+df = data.frame(1L:5L,seq(0,1,length.out=5), c("ab","cde","fghi","a","s"), stringAsFactors=FALSE)
+df
+h5write(df,"example.h5,"df")
+readA = h5read("example.h5","foo/A")
+readB = h5read("example.h5","foo/foobaa/B")
+readA = h5read("example.h5","df")
+
+readA
+h5write(c(12,13,14),"example.h5","foo/A",index=list(1:3,1))
+h5read("example.h5","foo/A")
+
+# ----------------------------------------------------------
+#       Web - API, authentification - Webscraping
+# ----------------------------------------------------------
+
+con = url("http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en")
+htmlCode = readLines(con)
+close(con)
+htmlCode
+
+h5write(df,"example.h5,"df")
+library(XML)
+url <- "http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en"
+html <- htmlTreeParse(url, useInternalNodes=T)
+
+xpathSApply(html,"//title",xmlValue)
+xpathSApply(html,"//td[@id='col-citedby']",xmlValue)
+
+url2 <- "http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
+html2 <- htmlTreeParse(url2, useInternalNodes=T)
+class(html2)
+html2
+xpathSApply(html2,"//title",xmlValue)
+
+library(httr)
+html2 = GET(url)
+content2 = content(html2,as="text")
+parsedHtml = htmlParse(content2, asText=T)
+xpathSApply(parsedHtml,"//title",xmlValue)
+
+pp1 = GET("http://httpbin.org/basic-auth/user/passwd")
+pp1
+
+pp2 = GET("http://httpbin.org/basic-auth/user/passwd", authenticate("user","passwd"))
+pp2
+
+names(pp2)
+
+google = handle("http://google.com")
+pg1 = GET(handle=google,path="/")
+pg2 = GET(handle=google,path="search")
+# save authent accros multiple acces
+
+# www.r-bloggers.com   search Web Scrapping
+
+# ----------------------------------------------------------
+#       API
+# ----------------------------------------------------------
+
+myapp = oauth_app("twitter",
+                  key="p0VwqpAdSAHIxqCzA2OCfJZAm",
+                  secret="BbEauZP2fQe3xuNdI2hN4paBGqAjn4cgYes7C1q7leA29tX19a")
+
+sig = sign_oauth1.0(myapp,
+                    token = "77304605-IwwHuRITl8nenpykgdZiw2ZbzMPSYBsXCxyPDs7zm",
+                    token_secret = "SQMcscNwnFEKVAqfFtfCgW8HQbq3UwL9gZZgE4stshW25")
+
+homeTL = GET("https://api.twitter.com/1.1/statuses/home_timeline.json",sig)
+library(jsonlite)
+
+json1 = content(homeTL)
+json2 = jsonlite::fromJSON(toJSON(json1))
+json2[1,1:4]
+
+# ----------------------------------------------------------
+#       other sources
+# ----------------------------------------------------------
+
+
