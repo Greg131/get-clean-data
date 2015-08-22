325f013b78ccbe3206906050474dfa4b86ab7200
Greg131 <gregoire_argenton@yahoo.fr>
2015-08-22 16:38:27 +0200

    week3

--- Script.R
+++ Script.R
@@ -3,7 +3,7 @@
 # ----------------------------------------------------------
 
 setwd("~/Dropbox/Datascience/Universite_Johns-Hopkins/Obtenir_et_trier_les_donnees/Repo")
-# Absolute setwd("../") ou relative setwd("./") ou ..~/ relative au user...
+# Absolute  ou relative setwd("./")  setwd("../") ou "~/" relative au user...
 getwd() #       Get working directory
 
 if (!file.exists("data"))       {
@@ -18,6 +18,7 @@ if (!file.exists("data"))       {
 fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
 download.file(fileUrl, destfile = "./data/cameras.csv", method = "curl")
 list.files("./data")
+?download.file
 
 #       If the url starts with http ... ok, 
 #                       https -> need to set method="curl" with MAC
@@ -54,18 +55,10 @@ cameraData <- read.xlsx("./data/cameras.xlsx",sheetIndex=1,header=TRUE)
 colIndex <- 2:3
 rowIndex <- 1:4
 cameraDataSubset <- read.xlsx("./data/cameras.xlsx",sheetIndex=1,header=TRUE, colIndex=colIndex, rowIndex=rowIndex)
-#cameraData <- read.xlsx("./data/cameras.xlsx",sheetIndex=1,header=TRUE)
-#colIndex <- 2:3
-#rowIndex <- 1:4
-#cameraDataSubset <- read.xlsx("./data/Baltimore_Fixed_Speed_Cameras.xls",sheetIndex=1,header=TRUE, colIndex=colIndex, rowIndex=rowIndex)
 
-
-# ----------------------------------------------------------
 #       Writing Excel files : write.xlsx
 #       read.xls2 faster but instable for subset
 #       XLconnect package ... more options
-# ----------------------------------------------------------
-
 
 # ----------------------------------------------------------
 #       Reading XML
@@ -200,6 +193,7 @@ DF[c(2,3)]  # selectionne les colonnes du data.frame
 
 DF[,c(2,3)]   #   Subsetting colonnes 2 et 3 du data.frame
 DT[,c(2,3)]   #   the argument you pass is an "expression"
+DT[,c(2,33)]   
 # ie collection of statement enclosed in curly bracket
 {
   x=1
@@ -210,6 +204,7 @@ print(k)
 
 DT[,list(mean(x),sum(z))]   #   liste de function avec les colonnes concern??es
 DT[,table(y)]
+table(DF$y)
 
 #     Adding new column
 DT[,w:=z^2]
@@ -219,7 +214,9 @@ DT3 <- DT   # Attention copie non effectu??e ... bien pour memoire mais
 # changements de DT changent DT3
 DT[, y:=2]
 
-
+tables()
+DT
+DT3
 #     Multiple operations
 DT[,m:= {tmp <- (x+z); log2(tmp+5)}]
 DT
@@ -423,12 +420,12 @@ A
 #h5write(A,"example.h5,"foo/A"")
 B = array(seq(0.1,2.0,by = 0.1),dim=c(5,2,2))
 B
-#h5write(B,"example.h5,"foo/foobaa/B"")
-#h5ls("example.h5")
+# h5write(B,"example.h5,"foo/foobaa/B"")
+# h5ls("example.h5")
 
-df = data.frame(1L:5L,seq(0,1,length.out=5), c("ab","cde","fghi","a","s"), stringAsFactors=FALSE)
-df
-h5write(df,"example.h5,"df")
+# df = data.frame(1L:5L,seq(0,1,length.out=5), c("ab","cde","fghi","a","s"), stringAsFactors=FALSE)
+# df
+# h5write(df,"example.h5,"df")
 #readA = h5read("example.h5","foo/A")
 #readB = h5read("example.h5","foo/foobaa/B")
 #readA = h5read("example.h5","df")
@@ -445,8 +442,21 @@ con = url("http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en")
 htmlCode = readLines(con)
 close(con)
 htmlCode
+class(htmlCode)
+length(htmlCode)
+
+con = url("http://www.duclert.org/Aide-memoire-R/Le-langage/Chaines-de-caracteres.php")
+htmlCode = readLines(con)
+close(con)
+htmlCode
+class(htmlCode)
+length(htmlCode)
+
+?readLines
+# Read Text Lines from a Connection
 
 #h5write(df,"example.h5,"df")
+
 library(XML)
 url <- "http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en"
 html <- htmlTreeParse(url, useInternalNodes=T)
@@ -464,6 +474,8 @@ library(httr)
 
 html2 = GET(url)
 content2 = content(html2,as="text")
+class(content2)
+length(content2)
 parsedHtml = htmlParse(content2, asText=T)
 xpathSApply(parsedHtml,"//title",xmlValue)
 
@@ -762,4 +774,543 @@ for (i in 1:nrow) {
   sum <- sum + tmp
 } 
 sum
+
+
+# ----------------------------------------------------------
+#       Week 3
+# ----------------------------------------------------------
+
+#       Subsetting and sorting
+
+set.seed(13435)
+?sample()
+sample(1:5)
+sample(1:5,3)
+sample(1:5,size=3, replace= T)
+
+X <- data.frame("var1"=sample(1:5),"var2"=sample(6:10),"var3"=sample(11:15))
+X
+X <- X[sample(1:5),]
+X
+X$var2[c(1,3)] = NA
+X
+
+X[,1]
+X[,"var1"]
+X[,"var1", drop=F]
+X[1:2,"var2"]
+X[1:2,c("var2","var1")]
+
+X[X$var1 <= 3 & X$var3 >11,]
+X[X$var1 <= 3 | X$var3 >15,]
+
+X
+X[X$var2>8,]  # probl??mes avec les missing values NA
+X[which(X$var2>8),] # elimine les NA...
+?which() # NA are allowed and treated as False...
+
+X$var1
+sort(X$var1)
+sort(X$var1,decreasing=TRUE)
+?sort()
+X
+sort(X$var2)
+sort(X$var2,na.last=TRUE)
+
+X
+order(X$var1) # retourne le vecteur d'indice dans l'odre de var1...
+
+X[order(X$var1),] #ordonne dans l'ordre de la variable 1
+X[order(X$var1,X$var3),] #ordonne dans l'ordre de la variable 1 puis 3 pour les 1 identiques
+
+library(plyr)
+X
+arrange(X,var1)   # re ordonne suivant var1 
+X
+arrange(X,desc(var1))
+?desc()
+desc(var1)
+arrange(X,var2) # traite les NA ?? la fin
+arrange(X,desc(var2)) # traite les NA ?? la fin dans tous les cas
+
+X$var4 <- rnorm(5)
+X
+
+Y <- cbind(X,rnorm(5))
+Y
+Y <- cbind(rnorm(5),X)
+Y
+
+Z <- rbind(rnorm(4),X) # conversion des types.... du coup...
+Z
+X
+
+#       Summarize data set
+
+fileUrl <- "https://data.baltimorecity.gov/api/views/k5ry-ef3g/rows.csv?accessType=DOWNLOAD"
+download.file(fileUrl, destfile = "./data/restaurants.csv", method = "curl")
+list.files("./data")
+?read.table()
+restData <- read.csv("./data/restaurants.csv")
+# restData2 <- read.table("./data/restaurants.csv", sep=",", header=T)
+head(restData,n=3)
+tail(restData,n=3) # les 3 dernieres lignes
+summary(restData)
+str(restData)
+quantile(restData$councilDistrict,na.rm=T)  # Pour les variables quantitatives
+quantile(restData$councilDistrict,probs=c(0.5,0.75,0.90),na.rm=T)  # Pour les variables quantitatives
+
+table(restData$zipCode,useNA="ifany") # Add NA with number of missing value!
+
+
+table(restData$councilDistrict,restData$zipCode)
+table(restData$councilDistrict,restData$zipCode, useNA="ifany")
+
+# Check for missing values
+sum(is.na(restData$councilDistrict))
+
+any(is.na(restData$councilDistrict))
+
+all(restData$zipCode >0)
+?all()
+X
+all(X$var2>0)
+all(X$var2>0, na.rm=T)
+
+class(is.na(restData))
+# is.na(restData) renvoi une matrice logique de la taille du dataset
+colSums(is.na(restData))
+all(colSums(is.na(restData))==0) # si TRUE il n'y a aucunes missing values... sinon au moins une
+
+table(restData$zipCode %in% c("21212"))
+
+table(restData$zipCode %in% c("21212","21213"))
+
+restData[restData$zipCode %in% c("21212","21213"),]
+
+
+data(UCBAdmissions)
+DF = as.data.frame(UCBAdmissions)
+summary(DF)
+
+# Cross Tab
+?xtabs()
+# Create a contingency table (optionally a sparse matrix) from cross-classifying factors, 
+# usually contained in a data frame, using a formula interface
+xt <- xtabs(Freq ~ Gender + Admit, data = DF)
+xt
+xt <- xtabs(Freq ~ Gender , data = DF)
+xt
+table(DF$Gender)
+sum(DF[DF$Gender=="Male","Freq"])
+sum(DF[DF$Gender=="Female","Freq"])
+
+# Flat Tab
+warpbreaks$replicate <- rep(1:9, len = 54)
+xt <- xtabs(breaks ~ .,  data = warpbreaks)
+xt
+# somme des breaks... pour chaque combinaison des autres variables
+# un peu difficille ?? lire
+
+ftable(xt)  # concatenation de pusieurs variable (facteurs) sur les axes
+
+# Size of data set
+
+fakeData = rnorm(1e5)
+object.size(fakeData)
+print(object.size(fakeData),units="Mb")
+
+# Creating new variable
+
+# Missingness indicator / Cutting up quant var / Applying transform
+fileUrl <- "https://data.baltimorecity.gov/api/views/k5ry-ef3g/rows.csv?accessType=DOWNLOAD"
+download.file(fileUrl, destfile = "./data/restaurants.csv", method = "curl")
+list.files("./data")
+?read.table()
+restData <- read.csv("./data/restaurants.csv")
+
+s1 <- seq(1,10, by=2) ; s1
+s2 <- seq(1,10, length=3) ; s2
+x <- c(1,3,8,25,100); seq(along = x)
+
+str(restData)
+restData$nearMe = restData$neighborhood %in% c("Roland Park","Homeland")
+table(restData$nearMe)
+# then used for subsetting the data set
+restData$zipWrong = ifelse(restData$zipCode <0, TRUE, FALSE)
+table(restData$zipWrong, restData$zipCode <0)
+?ifelse()
+
+?cut()
+restData$zipGroups = cut(restData$zipCode, breaks=quantile(restData$zipCode))
+table(restData$zipGroups)
+table(restData$zipGroups, restData$zipCode)
+class(restData$zipGroups)
+levels(restData$zipGroups)
+
+library(Hmisc)
+restData$zipGroups = cut2(restData$zipCode,g=4)
+table(restData$zipGroups)
+# plus simple ...pour diviser suivant les quantiles
+
+# creation de facteurs
+
+restData$zcf <- factor(restData$zipCode)
+restData$zcf[1:10]
+class(restData$zcf)
+
+yesno <- sample(c("yes","no"),size = 10, replace = TRUE)
+yesno
+yesnofac = factor(yesno) # ordre alphab par defaut
+yesnofac
+yesnofac = factor(yesno, levels=c("yes","no"))
+yesnofac
+
+
+as.numeric(yesnofac)
+
+# avec cut2 factor variable....
+
+library(Hmisc); library(plyr)
+restData2 = mutate(restData,zipGroups=cut2(zipCode,g=4))
+table(restData2$zipGroups)
+
+
+#   transformation
+x=pi
+x
+
+abs(x)
+sqrt(x)
+ceiling(x)
+floor(x)
+round(x,digits=2)
+signif(x,digits=2)
+cos(x)
+sin(x)
+log(x)
+log2(x)
+log10(x)
+exp(x)
+
+
+# Reshaping the data
+# to get tidy data
+
+# 1 each variable forms a column
+# 2 each obs forms a row
+# 3 each table/file store data about one kind of obs
+
+library(reshape2)
+head(mtcars)
+
+# Melting data frames
+
+mtcars$carnames <- rownames(mtcars)
+carMelt <- melt(mtcars, id=c("carnames","gear","cyl"),measure.vars=c("mpg","hp"))
+head(carMelt,n=3)
+tail(carMelt,n=3)
+
+class(carMelt)
+?melt()
+# en gros cr??e une colonne variable, et pour chaque id, var donne une val
+# empile les variables...
+carMelt3 <- melt(mtcars, id=c("gear"),measure.vars=c("mpg","hp"))
+head(carMelt3,n=3)
+tail(carMelt3,n=3)
+
+carMelt2 <- melt(mtcars, id=c("carnames"),measure.vars=c("mpg","hp"))
+head(carMelt2,n=3)
+tail(carMelt2,n=3)
+
+# Pour chaque cyl, fonction sur les variables 
+cylData <- dcast(carMelt, cyl ~ variable)
+cylData
+# count par defaut
+cylData <- dcast(carMelt, cyl ~ variable, mean)
+cylData
+
+
+
+head(InsectSprays)
+str(InsectSprays)
+InsectSprays
+tapply(InsectSprays$count,InsectSprays$spray,sum)
+InsectSprays$count
+
+spIns = split(InsectSprays$count,InsectSprays$spray)
+spIns
+
+sprCount = lapply(spIns,sum)
+sprCount
+
+unlist(sprCount)
+
+sapply(spIns,sum)
+
+# plyr package
+library(plyr)
+ddply(InsectSprays,.(spray),summarise,sum=sum(count))
+
+spraySums <- ddply(InsectSprays,.(spray),summarise,sum=ave(count,FUN=sum))
+?ave()
+dim(spraySums)
+head(spraySums)
+spraySums
+
+
+#--------------------------------
+# dplyr : managing Dataframe
+library(dplyr)
+#--------------------------------
+# arrange / filter / select / mutate / rename
+# by Hadley Wickham
+# provides a grammar for data manipulation
+
+chicago <- readRDS("./data/chicago.rds") 
+
+dim(chicago)
+str(chicago)
+names(chicago)
+
+colSums(is.na(chicago))
+all(colSums(is.na(chicago))==0)
+
+summary(chicago)
+
+
+
+
+# Select()
+
+head(select(chicago,city:dptp))
+class(select(chicago,city:dptp))
+# donne les colonnes entre city & dptp
+head(select(chicago,-(city:dptp)))
+# except les col entre city  et dptp
+
+# sinon
+i <- match("city", names(chicago))
+j <- match("dptp", names(chicago))
+head(chicago[,i:j])
+
+
+# filter()
+chic.f <- filter(chicago, pm25tmean2 > 30)
+head(chic.f,10)
+
+X
+test <- filter(X, var2 > 6)
+test
+# semble ne pas prendre les NA
+
+chic.f <- filter(chicago, pm25tmean2 > 30 & tmpd > 80)
+head(chic.f,10)
+?filter()
+# arrange
+
+chicago <- arrange(chicago,date)
+head(chicago)
+tail(chicago)
+chicago <- arrange(chicago,desc(date))
+head(chicago)
+tail(chicago)
+
+
+# rename
+chicago <- rename(chicago,pm25 = pm25tmean2, dewpoint = dptp)
+head(chicago)
+
+mean(chicago$pm25,na.rm = TRUE)
+sd(chicago$pm25,na.rm = TRUE)
+
+# mutate() (transform existing var / create new var)
+chicago <- mutate(chicago, pm25detrend = pm25-mean(pm25, na.rm = TRUE)) # centrer la var
+mean(chicago$pm25detrend,na.rm = TRUE)
+
+chicago <- mutate(chicago, pm25detrend = (pm25-mean(pm25, na.rm = TRUE))/sd(pm25,na.rm = TRUE))
+?sd()
+head(chicago)
+head(select(chicago,pm25, pm25detrend))
+
+mean(chicago$pm25detrend,na.rm = TRUE)
+sd(chicago$pm25detrend,na.rm = TRUE)
+
+# group by
+chicago <- mutate(chicago, tempcat = factor( 1 * (tmpd > 80), labels = c("cold","hot")))
+table(chicago$tempcat)
+
+
+hotcold <- group_by(chicago, tempcat)
+hotcold
+class(hotcold)
+summarise(hotcold)
+summary(hotcold)
+summarise(hotcold, pm25 = mean(pm25), o3 = max(o3tmean2), no2 = median(no2tmean2))
+summarise(hotcold, pm25 = mean(pm25,na.rm=TRUE), o3 = max(o3tmean2), no2 = median(no2tmean2))
+
+summarize(hotcold, pm25 = mean(pm25,na.rm=TRUE), o3 = max(o3tmean2), no2 = median(no2tmean2))
+
+summarize(hotcold, sum(!is.na(pm25)), o3 = max(o3tmean2), no2 = median(no2tmean2))
+
+
+chicago <- mutate(chicago, year = as.POSIXlt(date)$year + 1900)
+years <- group_by(chicago,year)
+summarise(years, pm25 = mean(pm25,na.rm=TRUE), o3 = max(o3tmean2), no2 = median(no2tmean2))
+
+# special operator %>% pipe line
+chicago %>% mutate(month = as.POSIXlt(date)$mon + 1) %>% group_by(month) %>% summarise(pm225 = mean(pm25, na.rm = TRUE), o3 = max(o3tmean2), no2 = median(no2tmean2))
+
+
+
+
+# Merging Data???
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl1 = "https://dl.dropboxusercontent.com/u/7710864/data/reviews-apr29.csv"
+fileUrl2 = "https://dl.dropboxusercontent.com/u/7710864/data/solutions-apr29.csv"
+download.file(fileUrl1,destfile="./data/reviews.csv",method="curl")
+download.file(fileUrl2,destfile="./data/solutions.csv",method="curl")
+reviews = read.csv("./data/reviews.csv"); solutions <- read.csv("./data/solutions.csv")
+head(reviews,2)
+
+names(reviews)
+names(solutions)
+
+# important parameters x,y,by,by.x,by.y,all
+mergedData = merge(reviews,solutions,by.x="solution_id",by.y="id", all = TRUE) # all pour ajouter row avec de NA si dans un seul..
+head(mergedData)
+
+intersect(names(solutions),names(reviews))
+mergedData2 = merge(reviews,solutions,all=TRUE)
+head(mergedData2)
+mergedData3 = merge(reviews,solutions,all=FALSE)
+head(mergedData3)
+
+# Using Join in the plyr package
+# faster but less full featured
+
+df1 = data.frame(id=sample(1:10),x=rnorm(10))
+df2 = data.frame(id=sample(1:10),y=rnorm(10))
+arrange(join(df1,df2),id)
+# les nom doivent etre dans le 2 data set
+
+df1 = data.frame(id=sample(1:10),x=rnorm(10))
+df2 = data.frame(id=sample(1:10),y=rnorm(10))
+df3 = data.frame(id=sample(1:10),z=rnorm(10))
+dfList = list(df1,df2,df3)
+join_all(dfList)
+# id commun le 3 data set
+
+# ----------------------------------------------------------
+#       Quizz Week 3
+# ----------------------------------------------------------
+
+# The American Community Survey distributes downloadable data 
+# about United States communities. 
+# Download the 2006 microdata survey about housing for the state 
+# of Idaho using download.file() from here:  
+#  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv 
+# and load the data into R. 
+# The code book, describing the variable names is here: 
+#  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf 
+
+# Create a logical vector that identifies the households on 
+# greater than 10 acres who sold more than $10,000 worth of 
+# agriculture products. Assign that logical vector to the variable 
+# agricultureLogical. Apply the which() function like this to 
+# identify the rows of the data frame where the logical vector is TRUE. 
+# which(agricultureLogical) What are the first 3 values that result?
+
+if (!file.exists("data"))       {
+  dir.create("data")
+}
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(fileUrl,"./data/housing2006idaho.csv", method = "curl")
+housing <- read.csv("./data/housing2006idaho.csv")
+
+str(housing)
+agricultureLogical <- (housing$ACR == 3 ) & (housing$AGS == 6 )
+which(agricultureLogical)
+
+
+
+# Question 2
+# Using the jpeg package read in the following picture of your instructor into R 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg 
+# Use the parameter native=TRUE. What are the 30th and 80th quantiles 
+# of the resulting data? 
+# (some Linux systems may produce an answer 638 different for the 30th quantile)
+
+library(jpeg)
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
+download.file(fileUrl,"./data/image.jpg", method = "curl")
+
+image <- readJPEG("./data/image.jpg", native = TRUE)
+class(image)
+class(unclass(image))
+dim(image)
+?quantile()
+quantile(image, probs=c(0,0.3,0.8,1))
+quantile(image, probs=c(0.3,0.8))
+
+
+# Question 3
+# Load the Gross Domestic Product data for the 190 ranked countries in this data set: 
+#  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv 
+# Load the educational data from this data set: 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv 
+# Match the data based on the country shortcode. 
+# How many of the IDs match? Sort the data frame in descending 
+# order by GDP rank (so United States is last). 
+# What is the 13th country in the resulting data frame? 
+
+# Original data sources: 
+#  http://data.worldbank.org/data-catalog/GDP-ranking-table 
+#  http://data.worldbank.org/data-catalog/ed-stats
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+download.file(fileUrl,"./data/GDP.csv", method = "curl")
+GDP <- read.csv("./data/GDP.csv",skip = 4,nrow = 231)
+
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+download.file(fileUrl,"./data/edu.csv", method = "curl")
+edu <- read.csv("./data/edu.csv")
+
+
+names(edu)
+names(GDP)
+
+library(dplyr)
+GDP <- rename(GDP,CountryCode = X, Rank = X.1, Name = X.3, gdp = X.4)
+summary(GDP$Rank)
+GDP <- GDP[!is.na(GDP$Rank),]
+
+GDPEDU = merge(GDP,edu,by.x="CountryCode",by.y="CountryCode", all = FALSE) 
+head(GDPEDU)
+GDPEDU <- arrange(GDPEDU,desc(Rank))
+
+
+# Question 4
+
+?table()
+table(GDPEDU$Income.Group,useNA="ifany")
+summary(GDPEDU$Income.Group)
+str(GDPEDU$Income.Group)
+
+tapply(GDPEDU$Rank,GDPEDU$Income.Group,mean)
+
+
+#GDPEDU$RankGroups = cut(GDPEDU$Rank, breaks=quantile(GDPEDU$Rank))
+GDPEDU$RankGroups = cut(GDPEDU$Rank, breaks=quantile(GDPEDU$Rank, probs=c(0,0.2,0.4,0.6,0.8,1),na.rm=T))
+table(GDPEDU$RankGroups)
+levels(GDPEDU$RankGroups)
+GDPEDU$gdp
+answer <- group_by(GDPEDU, Income.Group)
+summarize(answer,nb = mean(gdp))
+
+names(GDPEDU)
+table(GDPEDU$Income.Group,GDPEDU$RankGroups)
 
